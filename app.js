ba0f704bb3d1fee1b43be5b9b61805e4401c0779
arnaumv <95907269+arnaumv@users.noreply.github.com>
2023-09-25 19:46:49 +0200

    app.js

--- app.js
+++ app.js
@@ -31,3 +31,43 @@ console.log('Received kill signal, shutting down gracefully');
 httpServer.close()
 process.exit(0);
 }
+
+
+
+// Configurar direcció ‘/llistat’ i paràmetres URL 
+app.get('/llistat', getLlistat)
+async function getLlistat (req, res) {
+ let query = url.parse(req.url, true).query;
+  if (query.cerca && query.color) {
+   res.send(`Aquí tens el llistat de ${query.cerca} de color ${query.color}`)
+  } else {
+   res.send('Paràmetres incorrectes')
+  }
+}
+
+
+
+// Retornar una pàgina dinàmica de item
+app.get('/item', getItem)
+async function getItem (req, res) {
+  let query = url.parse(req.url, true).query;
+  try {
+  // Llegir el fitxer JSON
+  let dadesArxiu = await fs.readFile("./private/productes.json", { encoding: 'utf8'})
+  console.log(dadesArxiu)
+  let dades = JSON.parse(dadesArxiu)
+  console.log(dades)
+  // Buscar la nau per nom
+  let infoProd = dades.find(nau => (nau.nom == query.nom))
+  if (infoProd) {
+  // Retornar la pàgina segons la nau trobada
+  // Fa servir la plantilla 'sites/item.ejs'
+  res.render('sites/item', { infoProd: infoProd })
+  } else {
+  res.send('Paràmetres incorrectes')
+  }
+  } catch (error) {
+  console.error(error)
+  res.send('Error al llegir el fitxer JSON')
+  }
+  }
